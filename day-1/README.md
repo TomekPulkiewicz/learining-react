aa95f290f5b7a07a9d6732932294a479e997f4b8
Tomek Pulkiewicz <tomek@pulkiewicz.com>
2021-05-31 21:27:30 +0200

    Day 1 - First Commponent

---
+++ day-1/README.md
@@ -0,0 +1,8 @@
+# Day 1
+
+Welcome to the first day today i created my sample project using 
+```sh 
+npx create-react-app
+```
+And created my first component
+
---
+++ day-1/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>
+          My journey to learn React
+        </h2>
+        <p>Let's go</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
